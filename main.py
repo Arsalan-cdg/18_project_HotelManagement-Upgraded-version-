0c2d5cffb997c7ba26205985d07ffa16f81ba052
Arsalan <mdarsalan0002@gmail.com>
2026-09-18 19:15:08 +0530

    Implement Hotel Management System with room, customer, and booking functionalities and save and load functionalities

--- main.py
+++ main.py
@@ -53,7 +53,9 @@ class HotelManagement:
         self.rooms = []
         self.customers = []
         self.bookings = []
-        # load
+        self.LoadRoomsData()
+        self.LoadCustomersData()
+        self.LoadBookingsData()
     def GenerateRoomID(self): 
         if not self.rooms:
             return "R101"
@@ -105,23 +107,15 @@ class HotelManagement:
             }
         price = prices[room_type]
         return price
-    def InputRoomStatus(self):
-        while True:
-            status = input("Enter Room Status : ").upper()
-            if status == "AVAILABLE" or status == "OCCUPIED":
-                break
-            else:
-                print("Invalid Status")
-        return status
     def AddRoom(self):
         room_id = self.GenerateRoomID()
         room_no = self.InputRoomNo() 
         room_type = self.InputRoomType()
         price = self.RoomPrice(room_type)
-        room_status = self.InputRoomStatus()
+        room_status = "AVAILABLE"
         room = Rooms(room_id,room_no,room_type,price,room_status)
         self.rooms.append(room)
-        #save
+        self.SaveRoomsData()
     def ViewRooms(self):
         if self.CheckRooms():
             for obj in self.rooms:
@@ -203,7 +197,7 @@ class HotelManagement:
         phone_no = self.InputPhoneNo()
         customer = Customers(customer_id,name,age,phone_no)
         self.customers.append(customer)
-        #save
+        self.SaveCustomersData()
     def GenerateBookingID(self):
         if not self.bookings:
             return "B10001"
@@ -232,14 +226,17 @@ class HotelManagement:
                     if not obj:
                         print("Room Not Found")
                     else:
-                        if obj[0].status == "OCCUPIED":
+                        obj = obj[0]
+                        if obj.status == "OCCUPIED":
                             print("Room Is Currrently Occupied")
                         else:
-                            obj[0].status = "OCCUPIED"
+                            obj.status = "OCCUPIED"
+                            self.SaveRoomsData()
                             break
                 else:
-                    print("Invalid Room No") 
-        return obj[0].room_id
+                    print("Invalid Room No")
+        return obj.room_id
+
     def BookingDate(self):
         while True:
             check_in_date = input("Enter Date in Which You Want to Book Room (YYYY-MM-DD): ")
@@ -274,10 +271,10 @@ class HotelManagement:
             room_id = self.CheckRoomID()
             check_in_date = self.BookingDate()
             check_out_date = self.CalculateDate(check_in_date)
-            booking_status = "Booked"
+            booking_status = "BOOKED"
             booking = Bookings(booking_id,customer_id,room_id,check_in_date,check_out_date,booking_status)
             self.bookings.append(booking)
-            #save
+            self.SaveBookingsData()
     def CheckInAndOut(self):
         booking_id = input("Enter Booking ID : ")
         if booking_id.isalnum():
@@ -285,23 +282,26 @@ class HotelManagement:
             if not obj:
                 print("Invalid Booking ID")
             else:
-                if date.today() >= obj[0].check_in_date < obj[0].check_out_date:
-                    obj[0].booking_status = "Check In"
+                obj = obj[0]
+                if date.today() == obj.check_in_date:
+                    obj.booking_status = "CHECK IN"
                     print("You Check In")
-                elif date.today() == obj[0].check_out_date:
-                    obj[0].booking_status = "Completed"
+                    self.SaveBookingsData()
+                elif date.today() == obj.check_out_date:
+                    obj.booking_status = "COMPLETED"
                     print("You Check Out")
-                    room_id = obj[0].room_id
+                    print(self.GenerateBill(obj))
+                    room_id = obj.room_id
                     for obj in self.rooms:
                         if obj.room_id == room_id:
                             obj.status = "AVAILABLE"
-                    #Bill Should Be Atomaticaly Generate
+                    self.SaveBookingsData()
+                    self.SaveRoomsData()
                 else:
                     print("Please Check Your Check IN and Out Dates")
         else:
             print("Invalid Booking ID")
-        #save
-    def ViewInfo(self):
+    def ViewCustomerInfo(self):
         if self.CheckRooms():
             if self.customers:
                 name = self.InputName() 
@@ -310,11 +310,12 @@ class HotelManagement:
                 if not customer_obj:
                     print("Please Add Customer First")
                 else:
-                    customer_id = customer_obj[0].customer_id
+                    customer_obj = customer_obj[0]
+                    customer_id = customer_obj.customer_id
                     if self.bookings:
                         booking_obj = [obj for obj in self.bookings if obj.customer_id == customer_id]
                         if not booking_obj:
-                            print(customer_obj[0])
+                            print(customer_obj)
                             print("You Have Not Book Any Room Yet!")
                         else:
                             print(booking_obj[0])
@@ -322,13 +323,156 @@ class HotelManagement:
                         print("You Have Not Book Any Room Yet!")
             else:
                 print("Please Add Customer First")
-
-
-        
-
-
-                
-                    
-
+    def CancelBooking(self):
+        if not self.bookings:
+            print("There Are No Bookings")
+        else:
+            booking_id = input("Enter Your Booking ID : ")
+            if booking_id.isalnum():
+                booking_obj = [obj for obj in self.bookings if obj.booking_id == booking_id]
+                if not booking_obj:
+                    print("You Have Not Book any Room")
+                else:
+                    booking_obj = booking_obj[0]
+                    if booking_obj.booking_status == "BOOKED":
+                        choice = input("Do You Want to Cancel Your Booking(Y/N) : ").upper()
+                        if choice == "Y":
+                            booking_obj.booking_status = "CANCELLED"
+                            room_id = booking_obj.room_id
+                            room_obj = [obj for obj in self.rooms if obj.room_id == room_id]
+                            room_obj = room_obj[0]
+                            room_obj.status = "AVAILABLE"
+                            print("Your Booking is Successfully Cancelled")
+                            self.SaveBookingsData()
+                            self.SaveRoomsData()
+                        elif choice == "N":
+                            print("OK,Your Booking is Still Active")
+                        else:
+                            print("Invalid Choice")
+                    else:
+                        print("You Can't Cancel Your Booking")
+            else:
+                print("Invalid Booking ID")
+    def GenerateBill(self,booking_obj):
+        room_id = booking_obj.room_id
+        customer_id = booking_obj.customer_id
+        room_obj = [obj for obj in self.rooms if obj.room_id == room_id]
+        room_obj = room_obj[0]
+        customer_obj = [obj for obj in self.customers if obj.customer_id == customer_id]
+        customer_obj = customer_obj[0]
+        check_in_date = booking_obj.check_in_date
+        check_out_date = booking_obj.check_out_date
+        stay = int((str(check_out_date - check_in_date).split(" "))[0])
+        room_price = room_obj.price
+        bill = stay*room_price
+        output = f'''
+        Your Bill ------------------------------------
+        Name : {customer_obj.name}
+        Customer ID : {customer_obj.customer_id}
+        Booking ID : {booking_obj.booking_id}
+        Room ID : {room_obj.room_id}
+        Room Type : {room_obj.room_type}
+        Room Price/Night : {room_price} Rs
+        Check In Date : {check_in_date}
+        Check Out Date : {check_out_date}
+        You Stay Here : {stay} Days
+        ----------------------------------------------
+        Total Bill : {bill} Rs
+        '''
+        return textwrap.dedent(output).strip()
+    def SaveRoomsData(self):
+        rooms_data = {}
+        for obj in self.rooms:
+            rooms_data[obj.room_id] = {"Room No":obj.room_no,"Room Type":obj.room_type,"Status":obj.status,"Price":obj.price}
+        with open("rooms_data.json","w") as f:
+            json.dump(rooms_data,f,indent=4)
+    def SaveCustomersData(self):
+        customers_data = {}
+        for obj in self.customers:
+            customers_data[obj.customer_id] = {"Name":obj.name,"Age":obj.age,"Phone No":obj.phone_no}
+        with open("customers_data","w") as f:
+            json.dump(customers_data,f,indent=4)
+    def SaveBookingsData(self):
+        bookings_data = {}
+        for obj in self.bookings:
+            bookings_data[obj.booking_id] = {"Customer ID":obj.customer_id,"Room ID":obj.room_id,"Check In Date":obj.check_in_date,"Check Out Date":obj.check_out_date,"Booking Status":obj.booking_status}
+        with open("bookings_data","w") as f:
+            json.dump(bookings_data,f,indent=4)
+    def LoadRoomsData(self):
+        try:
+            with open("rooms_data") as f:
+                rooms_data = json.load(f)
+            for id,info in rooms_data.items():
+                room = Rooms(id,info["Room No"],info["Room Type"],info["Price"],info["Status"])
+                self.rooms.append(room)
+        except FileNotFoundError,json.JSONDecodeError:
+            return
+    def LoadCustomersData(self):
+        try:
+            with open("customers_data") as f:
+                customers_data = json.load(f)
+            for id,info in customers_data.items():
+                customer = Customers(id,info["Name"],info["Age"],info["Phone No"])
+                self.customers.append(customer)
+        except FileNotFoundError,json.JSONDecodeError:
+            return
+    def LoadBookingsData(self):
+        try:
+            with open("bookings_data") as f:
+                bookings_data = json.load(f)
+            for id,info in bookings_data.items():
+                check_in_date = info["Check In Date"]
+                check_in_date = date.strptime(check_in_date,"%Y-%m-%d")
+                check_out_date = info["Check Out Date"]
+                check_out_date = date.strptime(check_out_date,"%Y-%m-%d")
+                booking = Bookings(id,info["Customer ID"],info["Room ID"],check_in_date,check_out_date,info["Booking Status"])
+                self.bookings.append(booking)
+        except FileNotFoundError,json.JSONDecodeError:
+            return
+hotel_management = HotelManagement()
+def menu():
+    print('''
+    1.Add Rooms
+    2.View Rooms
+    3.Search Rooms
+    4.Add Customer
+    5.Create Booking
+    6.Check IN or OUT
+    7.Customer Info
+    8.Cancel Booking
+    9.Exit
+    ''')
+def InputChoice():
+    while True:
+        try:
+            choice = int(input("Enter Your Choice : "))
+            break
+        except ValueError:
+            print("Only Numbers Allowed")
+    return choice
+while True:
+    menu()
+    InputChoice()
+    if choice == 1:
+        hotel_management.AddRoom()
+    elif choice == 2:
+        hotel_management.ViewRooms()
+    elif choice == 3:
+        hotel_management.SearchRooms()
+    elif choice == 4:
+        hotel_management.AddCustomer()
+    elif choice == 5:
+        hotel_management.Booking()
+    elif choice == 6:
+        hotel_management.CheckInAndOut()
+    elif choice == 7:
+        hotel_management.ViewCustomerInfo()
+    elif choice == 8:
+        hotel_management.CancelBooking()
+    elif choice == 9:
+        print("Exited")
+        break
+    else:
+        print("Invalid Choice")
 
 
